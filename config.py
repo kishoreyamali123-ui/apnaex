6f3c73bf15c2bbbb4debcafb45a8ffaa47661343
kishoreyamali123-ui <kishore.yamali123@gmail.com>
2026-03-12 19:01:49 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,24 @@
+import os
+
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_USERNAME = os.getenv("BOT_USERNAME")
+
+BOT_TEXT = os.getenv("BOT_TEXT", "GAJJU BHAI KA EXTRACTOR")
+
+OWNER_ID = int(os.getenv("OWNER_ID"))
+CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
+
+MONGO_URL = os.getenv("MONGO_URL")
+
+PREMIUM_LOGS = int(os.getenv("PREMIUM_LOGS", "0"))
+
+join = '<a href="https://t.me/RPSC_UPSC_CURRENT">✳️ JOIN BACKUP</a>'
+
+UNSPLASH_ACCESS_KEY = os.getenv("UNSPLASH_ACCESS_KEY", "")
+UNSPLASH_QUERY = os.getenv("UNSPLASH_QUERY", "")
+
+ADMIN_BOT_USERNAME = os.getenv("ADMIN_BOT_USERNAME", "")
+THUMB_URL = os.getenv("THUMB_URL", "")
